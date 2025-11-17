e99ea43466939d9c3600cf3e2ab80666faee541a
IAftabRehman <iamaftabrehman@gmail.com>
2025-11-17 22:15:30 +0500

    small updates

--- lib/Views/Presentation/TimeTableScreen.dart
+++ lib/Views/Presentation/TimeTableScreen.dart
@@ -28,7 +28,8 @@ class _TimeTableScreenState extends State<TimeTableScreen> {
         leading: IconButton(onPressed: () => Navigator.pop(context), icon: Icon(Icons.arrow_back, color: AppColors.backgroundColor)),
       ),
       body: Column(
-        children: [ 
+        children: [
+          MyContainer()
         ],
       ),
     );
