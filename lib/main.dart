250f0dc0a9ba215122b754e38ff8e8feca242b83
IAftabRehman <iamaftabrehman@gmail.com>
2025-11-22 21:14:26 +0500

    Comment on Main file

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    /// Provider Files
     return MultiProvider(
       providers: [
         ChangeNotifierProvider(
